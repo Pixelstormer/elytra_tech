8d5e5d3c9b065e733e97a315304ced231295bff5
Pixelstorm <thepixelstormer@gmail.com>
2023-05-17 15:21:03 +0100

    Add mid flight boosting

--- src/main/java/com/example/example_mod/ExampleMod.java
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -2,8 +2,20 @@ package com.example.example_mod;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.networking.api.PacketSender;
+import org.quiltmc.qsl.networking.api.ServerPlayConnectionEvents;
+import org.quiltmc.qsl.networking.api.ServerPlayNetworking;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.example.example_mod.ElytraTech.ElytraBoostType;
+
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -11,8 +23,23 @@ public class ExampleMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Elytra Test");
 
+	public static final Identifier MID_FLIGHT_BOOST_PACKET_ID = new Identifier("elytra_tech", "mid_flight_boost");
+
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+
+		ServerPlayConnectionEvents.INIT.register((handler, server) -> {
+			ServerPlayNetworking.registerReceiver(handler, MID_FLIGHT_BOOST_PACKET_ID,
+					ExampleMod::receiveMidFlightBoostPacket);
+		});
+	}
+
+	private static void receiveMidFlightBoostPacket(MinecraftServer server, ServerPlayerEntity player,
+			ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
+		server.execute(() -> {
+			ElytraTech tech = (ElytraTech) player;
+			tech.elytraTechBoost(ElytraBoostType.LookDirection);
+		});
 	}
 }
---
+++ src/main/java/com/example/example_mod/mixin/client/ClientPlayerEntityMixin.java
@@ -0,0 +1,51 @@
+package com.example.example_mod.mixin.client;
+
+import org.quiltmc.qsl.networking.api.PacketByteBufs;
+import org.quiltmc.qsl.networking.api.client.ClientPlayNetworking;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.example.example_mod.ElytraTech;
+import com.example.example_mod.ExampleMod;
+import com.example.example_mod.ElytraTech.ElytraBoostType;
+import com.mojang.authlib.GameProfile;
+
+import net.minecraft.client.input.Input;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
+	@Shadow
+	private Input input;
+
+	private boolean wasJumpingPreviousTick;
+	private boolean wasFallFlyingPreviousTick;
+
+	public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+		super(world, profile);
+		throw new AssertionError();
+	}
+
+	@Inject(method = "tickMovement", at = @At("HEAD"))
+	private void getPriorJumpState(CallbackInfo ci) {
+		this.wasJumpingPreviousTick = this.input.jumping;
+		this.wasFallFlyingPreviousTick = this.isFallFlying();
+	}
+
+	@Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isFallFlying()Z"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V"), to = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isTouchingWater()Z", ordinal = 3)))
+	private void performMidFlightBoost(CallbackInfo ci) {
+		boolean startedJumpingThisTick = this.input.jumping && !this.wasJumpingPreviousTick;
+		boolean startedFallFlyingThisTick = this.isFallFlying() && !this.wasFallFlyingPreviousTick;
+		if (startedJumpingThisTick && this.isFallFlying() && !startedFallFlyingThisTick) {
+			ElytraTech tech = (ElytraTech) this;
+			tech.elytraTechBoost(ElytraBoostType.LookDirection);
+			ClientPlayNetworking.send(ExampleMod.MID_FLIGHT_BOOST_PACKET_ID, PacketByteBufs.empty());
+		}
+	}
+}
