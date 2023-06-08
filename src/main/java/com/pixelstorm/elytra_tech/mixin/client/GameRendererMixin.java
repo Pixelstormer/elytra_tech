c231565ca38f7e8257dd9ea614244bf7db8f79fa
Pixelstorm <thepixelstormer@gmail.com>
2023-06-08 21:31:20 +0100

    Fix held item model rotation when freelooking

--- src/main/java/com/pixelstorm/elytra_tech/mixin/client/GameRendererMixin.java
+++ src/main/java/com/pixelstorm/elytra_tech/mixin/client/GameRendererMixin.java
@@ -1,5 +1,7 @@
 package com.pixelstorm.elytra_tech.mixin.client;
 
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,20 +15,25 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Axis;
+import net.minecraft.util.math.MathConstants;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
 	@Shadow
 	private Camera camera;
 
-	@Inject(method = "renderHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;bobViewWhenHurt(Lnet/minecraft/client/util/math/MatrixStack;F)V", ordinal = 0))
+	@Inject(method = "renderHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/network/ClientPlayerEntity;I)V"))
 	private void modifyHandMatrix(MatrixStack matrices, Camera camera, float tickDelta, CallbackInfo ci) {
 		Entity entity = camera.getFocusedEntity();
 		if (entity instanceof CanFreeLook freeLooker && freeLooker.isFreeLooking()) {
 			float pitchDiff = freeLooker.getFreeLookPitch() - entity.getPitch(tickDelta);
 			float yawDiff = freeLooker.getFreeLookYaw() - entity.getYaw(tickDelta);
+
+			Vector3f axis = new Vector3f(0.0f, 1.0f, 0.0f)
+					.rotateX(freeLooker.getFreeLookPitch() * MathConstants.RADIANS_PER_DEGREE);
+
+			matrices.multiply(new Quaternionf().rotationAxis(yawDiff * MathConstants.RADIANS_PER_DEGREE, axis));
 			matrices.multiply(Axis.X_POSITIVE.rotationDegrees(pitchDiff));
-			matrices.multiply(Axis.Y_POSITIVE.rotationDegrees(yawDiff));
 		}
 	}
 }
